e1ec2b831c72e8901a95e0105d63bcc5834fb548
mohd irwan bin mohi <48343630+irwanmohi@users.noreply.github.com>
2021-08-10 01:12:18 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "# Script Auto Install SSH & OpenVPN             #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & 10 Buster 64 bit               #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Modified by IRWAN MOHI                        #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# IRWAN MOHI                                    #"
+echo -e "# Allah                                         #"
+echo -e "# My Family                                     #"
+echo -e "# Google                                        #"
+echo -e "# Owner VPS                                     #"
+echo -e "# Aidan.MY                                      #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
